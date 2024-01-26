772f3a1b1561e61857d1f0b9d084276e23b51ca3
Kooshan <39622525+Kooshano@users.noreply.github.com>
2024-01-26 12:00:07 +0330

    Initial setups

--- Snake.ino
+++ Snake.ino
@@ -1,3 +1,12 @@
+//define 16*16 matrix for rgb led matrix
+#include <FastLED.h>
+#define NUM_LEDS 256
+#define DATA_PIN 6
+#define brightness 10
+int matrix[16][16];
+//0 for being off 1 for snake 2 for food
+int snake[256];
+int snake_length = 1;
 void setup() {
   // put your setup code here, to run once:
 
@@ -6,4 +15,20 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+void update_matrix(){
+    for(int i = 0; i < 16; i++){
+        for(int j = 0; j < 16; j++){
+        if(matrix[i][j] == 0){
+            leds[i*16+j] = CRGB::Black;
+        }
+        else if(matrix[i][j] == 1){
+            leds[i*16+j] = CRGB::Green;
+        }
+        else if(matrix[i][j] == 2){
+            leds[i*16+j] = CRGB::Red;
+        }
+        }
+    }
+    FastLED.show();
 }
