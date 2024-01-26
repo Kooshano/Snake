599780db695f5023f3f9201d45fab45edb5e2ef6
Kooshan <39622525+Kooshano@users.noreply.github.com>
2024-01-26 12:12:52 +0330

    Check

--- Snake.ino
+++ Snake.ino
@@ -4,12 +4,36 @@
 #define DATA_PIN 6
 #define brightness 10
 int matrix[16][16];
+CRGB leds[NUM_LEDS];
 //0 for being off 1 for snake 2 for food
-int snake[256];
-int snake_length = 1;
+int snake_length = 3;
+string direction[256] = [];
+int snake_x[256] = [];
+int snake_y[256] = [];
+int food_x;
+int food_y;
 void setup() {
-  // put your setup code here, to run once:
-
+    Serial.begin(9600);
+    // put your setup code here, to run once:
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(brightness);
+    FastLED.clear();
+    for(int i = 0; i < 16; i++){
+        for(int j = 0; j < 16; j++){
+            matrix[i][j] = 2;
+        }
+    }
+    update_matrix();
+    // for(int i = 0; i < snake_length; i++){
+    //     snake_x[i] = 8;
+    //     snake_y[i] = 8+i;
+    //     matrix[snake_x[i]][snake_y[i]] = 1;
+    //     direction[i] = "up";
+    // }
+    // food_x = 8;
+    // food_y = 8;
+    // matrix[food_x][food_y] = 2;
+    // update_matrix();
 }
 
 void loop() {
